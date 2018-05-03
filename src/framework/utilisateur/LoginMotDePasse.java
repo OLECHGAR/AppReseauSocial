851e5f8a56f8dd64224906f610d12875ea092ced
jonathan MONBEIG <jonathan.mbg@hotmail.com>
2018-05-03 17:09:51 +0200

    Abstraction.

--- src/framework/utilisateur/LoginMotDePasse.java
+++ src/framework/utilisateur/LoginMotDePasse.java
@@ -4,51 +4,55 @@
 package framework.utilisateur;
 // Start of user code (user defined imports)
 
+import framework.exception.NotNullException;
+
 // End of user code
 
 /**
- * Description of Authentification.
+ * Description of LoginMotDePasse.
  * 
  * @author monbeigj
  */
-public class Authentification {
+public class LoginMotDePasse implements Auhentification {
 	/**
 	 * Description of the property user.
 	 */
 	private Utilisateur user = null;
 
-	// Start of user code (user defined attributes for Authentification)
+	// Start of user code (user defined attributes for LoginMotDePasse)
 
 	// End of user code
 
 	/**
 	 * The constructor.
 	 */
-	public Authentification() {
-		// Start of user code constructor for Authentification)
+	public LoginMotDePasse(String login, String mdp) {
+		// Start of user code constructor for LoginMotDePasse)
 		super();
+
 		// End of user code
 	}
 
-	// Start of user code (user defined methods for Authentification)
+	// Start of user code (user defined methods for LoginMotDePasse)
 
 	// End of user code
 	/**
 	 * Returns user.
-	 * @return user 
+	 * 
+	 * @return user
 	 */
 	public Utilisateur getUser() {
 		return this.user;
 	}
 
 	/**
-	 * Sets a value to attribute user. 
-	 * @param newUser 
+	 * Sets a value to attribute user.
+	 * 
+	 * @param newUser
 	 */
-	public void setUser(Utilisateur newUser) {
-		if (this.user != null) {
-			this.user = null;
-		}
+	public void setUser(Utilisateur newUser) throws NotNullException {
+		if (newUser == null)
+			throw new NotNullException("Utilisateur");
 		this.user = newUser;
 	}
 
