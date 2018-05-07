454395d2429e18a7b67e7d1f13b75c91ffc5cc25
jonathan MONBEIG <jonathan.mbg@hotmail.com>
2018-05-07 17:06:02 +0200

    Rename authentification

---
+++ src/framework/utilisateur/Authentification.java
@@ -0,0 +1,17 @@
+package framework.utilisateur;
+
+//Start of user code (user defined imports)
+
+//End of user code
+
+/**
+ * Description of Authentification.
+ * 
+ * @author monbeigj
+ */
+public interface Authentification {
+
+	// Start of user code (user defined methods for Authentification)
+
+	// End of user code
+}
