049726a24d071df5e43428638362e349122d133e
FlorianPignard <florianpignard@gmail.com>
2018-05-11 12:05:57 +0200

    Ajout des fonctionnalitées pour les documents.

--- src/framework/transferable/Document.java
+++ src/framework/transferable/Document.java
@@ -2,7 +2,11 @@
  * 2018, All rights reserved.
  *******************************************************************************/
 package framework.transferable;
+
 // Start of user code (user defined imports)
+
+import java.io.File;
+import java.net.URI;
 
 import framework.exception.NotNullException;
 import framework.zonesPartages.ZonePartageSimple;
@@ -14,23 +18,40 @@ import framework.zonesPartages.ZonePartageSimple;
  * 
  * @author monbeigj
  */
-public class Document extends $Fichier {
-	// Start of user code (user defined attributes for Document)
+public class Document extends $Fichier<File> {
+    // Start of user code (user defined attributes for Document)
 
+    // End of user code
+
+    /**
+     * 
+     * @param zone
+     * @param path
+     * @throws NotNullException
+     */
+    public Document(ZonePartageSimple zone, String path)
+	    throws NotNullException {
+	// Start of user code constructor for Document)
+	super(zone);
+	this.contenu = new File(path);
 	// End of user code
+    }
 
-	/**
-	 * The constructor.
-	 * @throws NotNullException 
-	 */
-	public Document(ZonePartageSimple zone) throws NotNullException {
-		// Start of user code constructor for Document)
-		super(zone);
-		// End of user code
-	}
-
-	// Start of user code (user defined methods for Document)
-
+    /**
+     * 
+     * @param zone
+     * @param path
+     * @throws NotNullException
+     */
+    public Document(ZonePartageSimple zone, URI path) throws NotNullException {
+	// Start of user code constructor for Document)
+	super(zone);
+	this.contenu = new File(path);
 	// End of user code
+    }
+
+    // Start of user code (user defined methods for Document)
+
+    // End of user code
 
 }
