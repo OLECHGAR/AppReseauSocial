9a88040230eca6b887446b9bc3de59231cf9a08f
marvin AKUESON <marvin.akueson@etu.univ-grenoble-alpes.fr>
2018-05-11 14:30:40 +0200

    blank

--- src/framework/transferable/Video.java
+++ src/framework/transferable/Video.java
@@ -4,6 +4,8 @@
 package framework.transferable;
 // Start of user code (user defined imports)
 
+import java.net.URL;
+
 import framework.exception.NotNullException;
 import framework.zonesPartages.ZonePartageSimple;
 
@@ -16,7 +18,7 @@ import framework.zonesPartages.ZonePartageSimple;
  */
 public class Video extends $Fichier {
 	// Start of user code (user defined attributes for Video)
-
+	private final String name;
 	// End of user code
 
 	/**
@@ -24,10 +26,11 @@ public class Video extends $Fichier {
 	 * 
 	 * @throws NotNullException
 	 */
-	public Video(ZonePartageSimple zone) throws NotNullException {
+	public Video(ZonePartageSimple zone, URL path) throws NotNullException {
 		// Start of user code constructor for Video)
 		super(zone);
 		// End of user code
+		this.name = path.getFile();
 	}
 
 	// Start of user code (user defined methods for Video)
