7c731309054a8a2b45f91d7ae7dce63abed4f19b
Mathieu <Mathieu@Mathieu>
2018-05-11 17:49:35 +0200

    - Ajout de la fonctionnalité : Quand on ferme la vue "Création de salon", l'application ouvre la vue "Chat"

--- src/application/controllers/AjoutZoneController.java
+++ src/application/controllers/AjoutZoneController.java
@@ -29,7 +29,7 @@ import javafx.stage.Stage;
 
 public class AjoutZoneController implements Initializable {
 
-	@FXML 
+	@FXML
 	private AnchorPane an_menu;
 	@FXML
 	private JFXTextField namezone;
@@ -43,83 +43,91 @@ public class AjoutZoneController implements Initializable {
 	private JFXRadioButton public_zone;
 	@FXML
 	private FontAwesomeIconView btn_close;
-	
+
 	private Utilisateur user;
 	private ReseauSocial reseauSocial;
 	private ArrayList<Utilisateur> Users;
 	private ObservableList<String> items = FXCollections.observableArrayList();
 	final ToggleGroup radioGroup = new ToggleGroup();
-	
+
 	@FXML
-	void AjouterZone(MouseEvent event) throws IOException, SQLException  {
-		
+	void AjouterZone(MouseEvent event) throws IOException, SQLException {
+
 		JFXRadioButton selected = (JFXRadioButton) radioGroup.getSelectedToggle();
 		System.out.println(selected.getText());
-		
+
 		Users = new ArrayList<Utilisateur>();
 		boolean privacy = false;
-		if (selected.getText().equals("Public")) privacy = true; 
-		
+		if (selected.getText().equals("Public"))
+			privacy = true;
+
 		reseauSocial.openConnection();
 		System.out.println("hello");
-		for(int index=0 ; index < listUsers.getItems().size(); index++)
-		{
+		for (int index = 0; index < listUsers.getItems().size(); index++) {
 			Users.add(reseauSocial.getUser(listUsers.getItems().get(index)));
 		}
-		
+
 		System.out.println("hello");
-		
-		if(!(reseauSocial.ajoutSalonDiscussion(namezone.getText(), privacy, Users, user)))
-				System.out.println("RATÉ");
+
+		if (!(reseauSocial.ajoutSalonDiscussion(namezone.getText(), privacy, Users, user)))
+			System.out.println("RATÉ");
 		System.out.println("hello");
 		reseauSocial.closeConnection();
-		
+
 		Alert alert = new Alert(AlertType.INFORMATION);
 		alert.setTitle("Validation");
 		alert.setHeaderText("Room successfully created");
 		alert.showAndWait();
-		
-        FXMLLoader Loader = new FXMLLoader();
+
+		FXMLLoader Loader = new FXMLLoader();
 		Loader.setLocation(getClass().getResource("/application/views/Chat.fxml"));
 		Loader.load();
-		
+
 		ChatController controller = Loader.getController();
 		controller.setObjects(reseauSocial, user);
-		
+
 		Parent home_page_parent = Loader.getRoot();
-        Scene home_page_scene = new Scene(home_page_parent);
-        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        app_stage.setScene(home_page_scene);
-        app_stage.show(); 
+		Scene home_page_scene = new Scene(home_page_parent);
+		Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		app_stage.setScene(home_page_scene);
+		app_stage.show();
 	}
-	
+
 	@FXML
-	void AjouterUtilisateur(MouseEvent event) 
-	{
-		//System.out.println(search_user.getText());
+	void AjouterUtilisateur(MouseEvent event) {
+		// System.out.println(search_user.getText());
 		items.add(search_user.getText());
 		listUsers.setItems(items);
 		search_user.setText("");
 	}
-	
+
 	@FXML
 	void close(MouseEvent event) throws IOException, SQLException {
-		Node node = (Node) event.getSource();
-		Stage stage = (Stage) node.getScene().getWindow();
-		stage.close();
+		FXMLLoader Loader = new FXMLLoader();
+		Loader.setLocation(getClass().getResource("/application/views/Chat.fxml"));
+		Loader.load();
+
+		ChatController controller = Loader.getController();
+		controller.setObjects(reseauSocial, user);
+
+		Parent home_page_parent = Loader.getRoot();
+		Scene home_page_scene = new Scene(home_page_parent);
+		Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		app_stage.setScene(home_page_scene);
+		app_stage.show();
 	}
-	
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
 		private_zone.setToggleGroup(radioGroup);
-        public_zone.setToggleGroup(radioGroup);
-        
+		public_zone.setToggleGroup(radioGroup);
+
 	}
-	
+
 	public void setObjects(ReseauSocial reseauSocial, Utilisateur user) {
 		this.reseauSocial = reseauSocial;
-		this.user = user;		
+		this.user = user;
 	}
 
 }
