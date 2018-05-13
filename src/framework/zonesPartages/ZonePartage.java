4fcf6ffdaa061e7e1ab6168d1a2b3b98977089ce
FlorianPignard <florianpignard@gmail.com>
2018-05-13 11:51:43 +0200

    Java doc

--- src/framework/zonesPartages/ZonePartage.java
+++ src/framework/zonesPartages/ZonePartage.java
@@ -1,14 +1,14 @@
-
 package framework.zonesPartages;
+
 // Start of user code (user defined imports)
 
 import java.util.ArrayList;
 import java.util.Iterator;
 
-import framework.utilisateur.*;
 import framework.exception.Ensure;
 import framework.exception.Invariant;
 import framework.exception.NotNullException;
+import framework.utilisateur.$Utilisateur;
 
 // End of user code
 
@@ -18,211 +18,245 @@ import framework.exception.NotNullException;
  * @author monbeigj
  */
 public class ZonePartage extends ZonePartageSimple {
-	// Start of user code (user defined attributes for ZonePartage)
-	/**
-	 * Description of the property messages.
-	 */
-	protected ArrayList<? extends $Utilisateur<?>> utilisateursAutorises;
-	protected $Utilisateur<?> proprietaire;
-	protected boolean estPrivee;
-	protected String nom;
+    // Start of user code (user defined attributes for ZonePartage)
+    /**
+     * Description of the property messages.
+     */
+    protected ArrayList<? extends $Utilisateur<?>> utilisateursAutorises;
+    protected $Utilisateur<?> proprietaire;
+    protected boolean estPrivee;
+    protected String nom;
 
-	// End of user code
-	/**
-	 * Constructeur d'une zone priv�e sans Propri�taire (pour un chat priv� entre 2
-	 * personnes cr�� par d�faut par exemple)
-	 * 
-	 * @param utilisateursAutorises
-	 * @throws NotNullException
-	 */
-	public ZonePartage(ArrayList<? extends $Utilisateur<?>> utilisateursAutorises) throws NotNullException {
-		super();
-		if(utilisateursAutorises == null)throw new NotNullException("utilisateursAutorises", "ZonePartage.ZonePartage");
-		this.proprietaire = null;
-		this.estPrivee = true;
-		this.utilisateursAutorises = utilisateursAutorises;
-		ajouterUtilisateurs(utilisateursAutorises);
-	}
+    // End of user code
+    /**
+     * Constructeur d'une zone priv�e sans Propri�taire (pour un chat
+     * priv� entre 2 personnes cr�� par d�faut par exemple)
+     * 
+     * @param utilisateursAutorises
+     * @throws NotNullException
+     */
+    public ZonePartage(
+	    ArrayList<? extends $Utilisateur<?>> utilisateursAutorises)
+	    throws NotNullException {
+	super();
+	if (utilisateursAutorises == null)
+	    throw new NotNullException("utilisateursAutorises",
+		    "ZonePartage.ZonePartage");
+	this.proprietaire = null;
+	this.estPrivee = true;
+	this.utilisateursAutorises = utilisateursAutorises;
+	this.ajouterUtilisateurs(utilisateursAutorises);
+    }
 
-	/**
-	 * Constructeur d'une zone publique avec un nom
-	 * 
-	 * @param proprietaire
-	 * @throws NotNullException
-	 */
-	public ZonePartage($Utilisateur<?> proprietaire, String nom) throws NotNullException {
-		super();
-		if (proprietaire == null)
-			throw new NotNullException("$Utilisateur proprietaire", "ZonePartage.ZonePartage");
-		this.proprietaire = proprietaire;
-		this.estPrivee = false;
-		if (nom == null)
-			this.nom = "";
-		else
-			this.nom = nom;
-	}
+    /**
+     * Constructeur d'une zone publique avec un nom
+     * 
+     * @param proprietaire
+     * @throws NotNullException
+     */
+    public ZonePartage($Utilisateur<?> proprietaire, String nom)
+	    throws NotNullException {
+	super();
+	if (proprietaire == null)
+	    throw new NotNullException("$Utilisateur proprietaire",
+		    "ZonePartage.ZonePartage");
+	this.proprietaire = proprietaire;
+	this.estPrivee = false;
+	if (nom == null)
+	    this.nom = "";
+	else
+	    this.nom = nom;
+    }
 
-	/**
-	 * Constructeur d'une zone priv�e avec une liste d'utilisateurs autoris�s � la lire et �crire.
-	 * 
-	 * @param proprietaire
-	 * @param utilisateursAutorises
-	 * @throws NotNullException
-	 */
-	public <U extends $Utilisateur<Z>, Z extends ZonePartageSimple> ZonePartage(U proprietaire, ArrayList<U> utilisateursAutorises)
-			throws NotNullException {
-		super();
-		if (proprietaire == null)
-			throw new NotNullException("$Utilisateur proprietaire", "ZonePartage.ZonePartage");
-		this.proprietaire = proprietaire;
-		this.estPrivee = true;
-		this.utilisateursAutorises = utilisateursAutorises;
-		ajouterUtilisateurs(utilisateursAutorises);
-	}
+    /**
+     * Constructeur d'une zone priv�e avec une liste d'utilisateurs
+     * autoris�s � la lire et �crire.
+     * 
+     * @param proprietaire
+     * @param utilisateursAutorises
+     * @throws NotNullException
+     */
+    public <U extends $Utilisateur<Z>, Z extends ZonePartageSimple> ZonePartage(
+	    U proprietaire, ArrayList<U> utilisateursAutorises)
+	    throws NotNullException {
+	super();
+	if (proprietaire == null)
+	    throw new NotNullException("$Utilisateur proprietaire",
+		    "ZonePartage.ZonePartage");
+	this.proprietaire = proprietaire;
+	this.estPrivee = true;
+	this.utilisateursAutorises = utilisateursAutorises;
+	this.ajouterUtilisateurs(utilisateursAutorises);
+    }
 
-	/**
-	 * Constructeur d'une zone priv�e avec une liste d'utilisateurs autoris�s � la lire et �crire avec un nom.
-	 * 
-	 * @param proprietaire
-	 * @param utilisateursAutorises
-	 * @param nom
-	 * @throws NotNullException
-	 */
-	public <U extends $Utilisateur<Z>, Z extends ZonePartageSimple>ZonePartage(U proprietaire, ArrayList<U> utilisateursAutorises, String nom)
-			throws NotNullException {
-		super();
-		if (proprietaire == null)
-			throw new NotNullException("$Utilisateur proprietaire", "ZonePartage.ZonePartage");
-		this.proprietaire = proprietaire;
-		this.estPrivee = true;
-		this.utilisateursAutorises = utilisateursAutorises;
-		this.nom = nom;
-		ajouterUtilisateurs(utilisateursAutorises);
-	}
+    /**
+     * Constructeur d'une zone priv�e avec une liste d'utilisateurs
+     * autoris�s � la lire et �crire avec un nom.
+     * 
+     * @param proprietaire
+     * @param utilisateursAutorises
+     * @param nom
+     * @throws NotNullException
+     */
+    public <U extends $Utilisateur<Z>, Z extends ZonePartageSimple> ZonePartage(
+	    U proprietaire, ArrayList<U> utilisateursAutorises, String nom)
+	    throws NotNullException {
+	super();
+	if (proprietaire == null)
+	    throw new NotNullException("$Utilisateur proprietaire",
+		    "ZonePartage.ZonePartage");
+	this.proprietaire = proprietaire;
+	this.estPrivee = true;
+	this.utilisateursAutorises = utilisateursAutorises;
+	this.nom = nom;
+	this.ajouterUtilisateurs(utilisateursAutorises);
+    }
 
-	// Start of user code (user defined methods for ZonePartage)
-	/**
-	 * Renvoie le propri�taire de la zone.
-	 * 
-	 * @return prorietaire
-	 */
-	public $Utilisateur<?> getProprietaire() {
-		return this.proprietaire;
-	}
+    // Start of user code (user defined methods for ZonePartage)
+    /**
+     * Renvoie le propri�taire de la zone.
+     * 
+     * @return prorietaire
+     */
+    public $Utilisateur<?> getProprietaire() {
+	return this.proprietaire;
+    }
 
-	/**
-	 * Modifie le propri�taire de la Zone
-	 * 
-	 * @param proprietaire
-	 * @throws NotNullException
-	 * @ensure this.proprietaire == proprietaire
-	 */
-	public void setProprietaire($Utilisateur<?> proprietaire) throws NotNullException {
-		if (proprietaire == null)
-			throw new NotNullException("$Utilisateur propriétaire", "ZonePartage.setProprietaire");
-		this.proprietaire = proprietaire;
-		if(this.proprietaire != proprietaire)throw new Ensure("this.proprietaire == proprietaire","ZonePartage.setProprietaire");
-	}
+    /**
+     * Modifie le propri�taire de la Zone
+     * 
+     * @param proprietaire
+     * @throws NotNullException
+     * @ensure this.proprietaire == proprietaire
+     */
+    public void setProprietaire($Utilisateur<?> proprietaire)
+	    throws NotNullException {
+	if (proprietaire == null)
+	    throw new NotNullException("$Utilisateur propriétaire",
+		    "ZonePartage.setProprietaire");
+	this.proprietaire = proprietaire;
+	if (this.proprietaire != proprietaire)
+	    throw new Ensure("this.proprietaire == proprietaire",
+		    "ZonePartage.setProprietaire");
+    }
 
-	/**
-	 * Ajoute une liste d'utilisateur � autoriser
-	 * @param $Utilisateurs
-	 * @throw NotNullException
-	 * @invariant estAjoute(it.next()) == true
-	 */
-	public void ajouterUtilisateurs(ArrayList<? extends $Utilisateur<?>> utililsateurs) {
-		if(utililsateurs == null)throw new NotNullException("utilisateurs","ZonePartage.ajouterUtilisateurs");
-		Iterator<? extends $Utilisateur<?>> it = utililsateurs.iterator();
-		while (it.hasNext()) {
-			if(!(estAjoute(it.next())))throw new Invariant("estAjoute(it.next()) == true","ZonePartage.ajouterUtilisateur");
-		}
+    /**
+     * Ajoute une liste d'utilisateur � autoriser
+     * 
+     * @param $Utilisateurs
+     * @throw NotNullException
+     * @invariant estAjoute(it.next()) == true
+     */
+    public void ajouterUtilisateurs(
+	    ArrayList<? extends $Utilisateur<?>> utililsateurs) {
+	if (utililsateurs == null)
+	    throw new NotNullException("utilisateurs",
+		    "ZonePartage.ajouterUtilisateurs");
+	Iterator<? extends $Utilisateur<?>> it = utililsateurs.iterator();
+	while (it.hasNext()) {
+	    if (!(this.estAjoute(it.next())))
+		throw new Invariant("estAjoute(it.next()) == true",
+			"ZonePartage.ajouterUtilisateur");
 	}
-	
-	/**
-	 * V�rifie si un utilisateur appartient � la liste des utilisateurs autoris�s
-	 * Ajoute et vérifie si utilisateur est bien ajouté à la liste
-	 * 
-	 * @param utilisateur
-	 * @return boolean
-	 * @throw NotNullException
-	 */
-	public <T extends ZonePartageSimple>boolean estAjoute($Utilisateur<T> utilisateur) {
-		if(utilisateur == null)throw new NotNullException("utilisateur", "ZonePartage.estAutorise");
-		utilisateur.rejoindreZone((T)this);
-		Iterator<? extends $Utilisateur<?>> it = this.utilisateursAutorises.iterator();
-		while(it.hasNext()){
-			if(it.next() == utilisateur)
-				return true;
-		}	
-		return false;
-	}
-	
-	/**
-	 * Retourne la liste des utilisateurs autoris�s
-	 * 
-	 * @return ArrayList<? extends $Utilisateur>
-	 */
-	public ArrayList<? extends $Utilisateur<?>> getUtilisateursAutorises() {
-		return utilisateursAutorises;
-	}
+    }
 
-	/**
-	 * Modifie la visibilit� de la zone en publique
-	 * 
-	 * @return boolean
-	 * @ensure this.estPrivee == false
-	 */
-	public boolean setPublique() {
-		if (this.estPrivee == false)
-			return false;
-		this.utilisateursAutorises = new ArrayList<$Utilisateur<?>>(); // TODO check is OK
-		this.estPrivee = false;
-		if(this.estPrivee != false)throw new Ensure("estPrivee == false","ZonePartage.setPublique");
+    /**
+     * V�rifie si un utilisateur appartient � la liste des utilisateurs
+     * autoris�s Ajoute et vérifie si utilisateur est bien ajouté à la
+     * liste
+     * 
+     * @param utilisateur
+     * @return boolean
+     * @throw NotNullException
+     */
+    public <T extends ZonePartageSimple> boolean estAjoute(
+	    $Utilisateur<T> utilisateur) {
+	if (utilisateur == null)
+	    throw new NotNullException("utilisateur", "ZonePartage.estAutorise");
+	utilisateur.rejoindreZone((T) this);
+	Iterator<? extends $Utilisateur<?>> it = this.utilisateursAutorises
+		.iterator();
+	while (it.hasNext()) {
+	    if (it.next() == utilisateur)
 		return true;
 	}
+	return false;
+    }
 
-	/**
-	 * Modifie la visibilit� de la zone en priv�e
-	 * 
-	 * @return boolean
-	 * @ensure this.estPrivee == true
-	 */
-	public boolean setPrivee(ArrayList<? extends $Utilisateur<?>> utilisateursAutorises) {
-		if (utilisateursAutorises == null)throw new NotNullException("utilisateursAutorises","ZonePartage.setPrivee");
-		if (this.estPrivee == true)
-			return false;
-		this.utilisateursAutorises = utilisateursAutorises;
-		this.ajouterUtilisateurs(utilisateursAutorises);
-		this.estPrivee = true;
-		if(this.estPrivee != true)throw new Ensure("estPrivee == true","ZonePartage.setPrivee");
-		return true;
-	}
+    /**
+     * Retourne la liste des utilisateurs autoris�s
+     * 
+     * @return ArrayList<? extends $Utilisateur>
+     */
+    public ArrayList<? extends $Utilisateur<?>> getUtilisateursAutorises() {
+	return this.utilisateursAutorises;
+    }
 
-	/**
-	 * 
-	 * @return
-	 */
-	public String getNom() {
-		return this.nom;
-	}
+    /**
+     * Modifie la visibilit� de la zone en publique
+     * 
+     * @return boolean
+     * @ensure this.estPrivee == false
+     */
+    public boolean setPublique() {
+	if (this.estPrivee == false)
+	    return false;
+	// TODO check is OK
+	this.utilisateursAutorises = new ArrayList<$Utilisateur<?>>();
+	this.estPrivee = false;
+	if (this.estPrivee != false)
+	    throw new Ensure("estPrivee == false", "ZonePartage.setPublique");
+	return true;
+    }
 
-	/**
-	 * 
-	 * @param nom
-	 * @throws NotNullException
-	 */
-	public void setNom(String nom) throws NotNullException {
-		this.nom = nom;
-	}
+    /**
+     * Modifie la visibilit� de la zone en priv�e
+     * 
+     * @return boolean
+     * @ensure this.estPrivee == true
+     */
+    public boolean setPrivee(
+	    ArrayList<? extends $Utilisateur<?>> utilisateursAutorises) {
+	if (utilisateursAutorises == null)
+	    throw new NotNullException("utilisateursAutorises",
+		    "ZonePartage.setPrivee");
+	if (this.estPrivee == true)
+	    return false;
+	this.utilisateursAutorises = utilisateursAutorises;
+	this.ajouterUtilisateurs(utilisateursAutorises);
+	this.estPrivee = true;
+	if (this.estPrivee != true)
+	    throw new Ensure("estPrivee == true", "ZonePartage.setPrivee");
+	return true;
+    }
 
-	/**
-	 * 
-	 * @return
-	 */
-	public boolean getPrivacy() {
-		return this.estPrivee;
-	}
+    /**
+     * Renvoie le nom de la zone.
+     * 
+     * @return
+     */
+    public String getNom() {
+	return this.nom;
+    }
 
-	// End of user code
+    /**
+     * Change le nom de la zone.
+     * 
+     * @param nom
+     */
+    public void setNom(String nom) {
+	this.nom = nom;
+    }
+
+    /**
+     * Renvoie la visibilit�e de la zone.
+     * 
+     * @return
+     */
+    public boolean getPrivacy() {
+	return this.estPrivee;
+    }
+
+    // End of user code
 
 }
--- src/framework/zonesPartages/ZonePartageSimple.java
+++ src/framework/zonesPartages/ZonePartageSimple.java
@@ -1,13 +1,15 @@
 package framework.zonesPartages;
 
-import java.rmi.RemoteException;
-import java.rmi.server.UnicastRemoteObject;
 //Start of user code (user defined imports)
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-import framework.exception.*;
+import framework.exception.Ensure;
+import framework.exception.NotNullException;
+import framework.exception.Require;
 import framework.transferable.$Transferable;
+
 //End of user code
 
 /**
@@ -15,118 +17,125 @@ import framework.transferable.$Transferable;
  * 
  * @author pignardf
  */
-public class ZonePartageSimple{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	// Start of user code (user defined attributes for ZonePartage)
-	// TODO <?extends type de contenu dans trasnferable quand a sera clair>
-	protected ArrayList<$Transferable<?>> transferables = new ArrayList<$Transferable<?>>();
+public class ZonePartageSimple {
+    // Start of user code (user defined attributes for ZonePartage)
+    private static final long serialVersionUID = 1L;
+    protected ArrayList<$Transferable<?>> transferables = new ArrayList<$Transferable<?>>();
+
+    // End of user code
+
+    /**
+     * The constructor.
+     * 
+     * @throws RemoteException
+     */
+    public ZonePartageSimple() {
+	// Start of user code constructor for Reception)
+	super();
 	// End of user code
+    }
 
-	/**
-	 * The constructor.
-	 * @throws RemoteException 
-	 */
-	public ZonePartageSimple(){
-		// Start of user code constructor for Reception)
-		super();
-		// End of user code
+    // Start of user code (user defined methods for ZonePartageSimple)
+    /**
+     * Returns tous les transferable de la zone.
+     * 
+     * @return transferables
+     */
+    public <T> ArrayList<$Transferable<?>> getTransferables() {
+	return this.transferables;
+    }
+
+    /**
+     * Ajoute le transferable a la zone.
+     * 
+     * @param transferable
+     * @throws NotNullException
+     */
+    public void addTransferable($Transferable<?> transferable)
+	    throws NotNullException {
+	if (transferable == null)
+	    throw new NotNullException("$Transferable transferable",
+		    "addTransferable");
+	this.transferables.add(transferable);
+	if (!(this.trouverTransferable(transferable)))
+	    throw new Ensure("trouverTransferabke(transferable) == true",
+		    "addTransferable");
+
+    }
+
+    /**
+     * 
+     * @param index
+     * @return
+     * @throws Require
+     */
+    public <T> T afficherMessageIndex(int index) throws Require {
+	if (index >= this.transferables.size())
+	    throw new Require("index < transferable.size()",
+		    "afficherMessageIndex");
+	return (T) this.transferables.get(index).getContenu();
+    }
+
+    /**
+     * Cherche si un transferable est dans la zone.
+     * 
+     * @param transferable
+     * @return true si le transferable existe dans la zone, false sinon.
+     */
+    private boolean trouverTransferable($Transferable<?> transferable) {
+	Iterator<$Transferable<?>> it = this.getTransferables().iterator();
+	while (it.hasNext()) {
+	    if (it.next().equals(transferable)) {
+		return true;
+	    }
 	}
+	return false;
+    }
 
-	// Start of user code (user defined methods for ZonePartageSimple)
-	/**
-	 * Returns tous les transferable de la zone.
-	 * 
-	 * @return transferables
-	 */
-	public <T> ArrayList<$Transferable<?>> getTransferables() {
-		return this.transferables;
-	}
-
-	/**
-	 * 
-	 * @param transferable
-	 * @throws NotNullException
-	 */
-	public void addTransferable($Transferable<?> transferable) throws NotNullException {
-		if (transferable == null)
-			throw new NotNullException("$Transferable transferable", "addTransferable");
-		this.transferables.add(transferable);
-		if (!(this.trouverTransferable(transferable)))
-			throw new Ensure("trouverTransferabke(transferable) == true", "addTransferable");
-
-	}
-
-	/**
-	 * 
-	 * @param index
-	 * @return
-	 * @throws Require
-	 */
-	public <T> T afficherMessageIndex(int index) throws Require {
-		if (index >= this.transferables.size())
-			throw new Require("index < transferable.size()", "afficherMessageIndex");
-		return (T) this.transferables.get(index).getContenu();
-	}
-
-	/**
-	 * 
-	 * @param transferable
-	 * @return
-	 */
-	private boolean trouverTransferable($Transferable<?> transferable) {
-		Iterator<$Transferable<?>> it = this.getTransferables().iterator();
-		while (it.hasNext()) {
-			if (it.next().equals(transferable)) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	/**
-	 * 
-	 * @param nom
-	 * @return
-	 */
-	public $Transferable<?> trouverTransferable(String nom) {
-		Iterator<$Transferable<?>> it = this.getTransferables().iterator();
-		$Transferable<?> result = null;
-		while (it.hasNext()) {
-			result = it.next();
-			// TODO change to don't need exactly the same name
-			// TODO change to use contenu name and not toString
-			if (result.getContenu().toString().equals(nom)) {
-				return result;
-			}
-		}
+    /**
+     * Cherche si un transferable est dans la zone.
+     * 
+     * @param nom
+     * @return le transferable trouv� ou null sinon.
+     */
+    public $Transferable<?> trouverTransferable(String nom) {
+	Iterator<$Transferable<?>> it = this.getTransferables().iterator();
+	$Transferable<?> result = null;
+	while (it.hasNext()) {
+	    result = it.next();
+	    // TODO change to don't need exactly the same name
+	    // TODO change to use contenu name and not toString
+	    if (result.getContenu().toString().equals(nom)) {
 		return result;
+	    }
 	}
+	return result;
+    }
 
-	/**
-	 * 
-	 * @return
-	 */
-	public int size() {
-		if (this.transferables == null)
-			return 0;
-		return this.transferables.size();
-	}
+    /**
+     * Permet de connaitre la taille de la zone, sont nombre de transf�rable.
+     * 
+     * @return sont nombre de transf�rable.
+     */
+    public int size() {
+	if (this.transferables == null)
+	    return 0;
+	return this.transferables.size();
+    }
 
-	/**
-	 * 
-	 * @param t
+    /**
+     * Permet de supprimer un transferable sur la zone.
+     * 
+     * @param t
+     */
+    public void supprimer($Transferable<?> t) {
+	this.transferables.remove(this.transferables.indexOf(t));
+	/*
+	 * ?à décider : Requete de supression du transferable et de toutes les
+	 * requetes en rapport Ou alors juste ne pas attribuer le message à une
+	 * zone pour qu'il disparaisse de la circulation mais de le garder en
+	 * base de donée afin de garder un historique des logs
 	 */
-	public void supprimer($Transferable<?> t) {
-		this.transferables.remove(this.transferables.indexOf(t));
-		/*
-		 * ?à décider : Requete de supression du transferable et de toutes les
-		 * requetes en rapport Ou alors juste ne pas attribuer le message à une
-		 * zone pour qu'il disparaisse de la circulation mais de le garder en
-		 * base de donée afin de garder un historique des logs
-		 */
-	}
-	// End of user code
+    }
+    // End of user code
 }
